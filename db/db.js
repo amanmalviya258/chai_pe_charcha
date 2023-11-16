2b26406d3874b84d9f35aff416c158649c0d9ed0
Aman Malviya <amanmalviya258@gmail.com>
2023-11-16 18:50:07 +0530

    database connection

---
+++ db/db.js
@@ -0,0 +1,14 @@
+function connect() {
+  mongoose
+    .connect(process.env.MONDODB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+    .then(console.log("connected"))
+    .catch((err) => {
+      console.log(err);
+    });
+}
+
+
+modules.export = connect
