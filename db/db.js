21cce9ada5921d71a344a3ecbed9e1e8812f48f3
Aman Malviya <amanmalviya258@gmail.com>
2023-11-19 18:09:05 +0530

    added iffe

--- db/db.js
+++ db/db.js
@@ -1,14 +1,32 @@
-function connect() {
-  mongoose
-    .connect(process.env.MONDODB_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    })
-    .then(console.log("connected"))
-    .catch((err) => {
-      console.log(err);
-    });
-}
 
 
-modules.export = connect 
+
+
+
+// * ----------------------------------------------------------------
+
+//Method One 
+//  (async () => {
+//   try {
+//     mongoose.connect(process.env.MONDODB_URI);
+//   } catch (error) {
+//     console.log("ERROR", error);
+//     throw err;
+//   }
+// })();
+
+ 
+
+// *  ----------------------------------------------------------------
+
+//Method Two
+
+// function connect() {
+//   mongoose.connect(process.env.MONDODB_URI, {useNewUrlParser: true,  useUnifiedTopology: true,})
+//     .then(console.log("connected"))
+//     .catch((err) => {
+//       console.log(err);
+//     });
+// }
+
+// modules.export = connect
